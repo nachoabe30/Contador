4c89736c3809ba403a9da56f3c50aae40d38c216
Nacho <abellesenignacio@gmail.com>
2025-08-17 23:38:31 -0300

    Agregamos el segundo componente dentro de nuestro maquetado de React Contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Bienvenidos from "./components/Bienvenidos";
+import TeoriaComponente from "./components/TeoriaComponente";
 
 function App() {
   //Aqui agrego la logica de un componente
   return(
     //Aqui puedo agregar mas logica
-    <>
+    <main className="container my-5">
 <h1>Hola Mundo</h1>
-<Bienvenidos></Bienvenidos>
 <Bienvenidos/>
-  
-  </>
+<TeoriaComponente/>
+
+  </main>
   );
 
 }
