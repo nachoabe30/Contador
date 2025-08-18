ac554d88fa1c12224e7caeda0a62743ae50aa5f5
Nacho <abellesenignacio@gmail.com>
2025-08-17 21:10:32 -0300

    Agregamos contenido de react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Bienvenidos from "./components/Bienvenidos";
 
 function App() {
+  //Aqui agrego la logica de un componente
+  return(
+    //Aqui puedo agregar mas logica
+    <>
+<h1>Hola Mundo</h1>
+<Bienvenidos></Bienvenidos>
+<Bienvenidos/>
+  
+  </>
+  );
 
-  return <h1>Hola Mundo</h1>
 }
 
 export default App
---
+++ src/components/Bienvenidos.jsx
@@ -0,0 +1,10 @@
+const Bienvenidos = () => {
+    return (
+        <div>
+            <h2>Informacion de react</h2>
+            <p>React es una biblioteca de JavasCript desarrollada por Facebook para construir interfaces de usuario. Se destaca por su enfoqye en la creacion de aplicaciones web de una sola pagina SPA y su capacidad para manejar la interfaz de manera eficiente mediante el uso de un DOM virtual. <br />React permite a los desarrolladores construir interfaces dianmicas y reactivas de forma modular, lo qye facilita el desarrollo y el mantenimiento de aplicaciones a gran escala.</p>
+        </div>
+    );
+};
+
+export default Bienvenidos;
